c716bc96abb1a4d764c374a2e528829ddf9088e4
Enola Tutellier <enola.tutellier@hotmail.com>
2022-05-31 11:38:11 +0200

    .

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +8,23 @@
     <title>Header</title>
 </head>
 <body>
+<header>
+        <nav>
+            <ul>
+                <li> <!-- Accueil de la page -->
+                    <a href="index.html"> Accueil </a>
+                </li>
+
+                <li> <!-- A propos de moi, competences, parcour.. langage utilisé ?  -->
+                    <a href="index.html"> A propos </a>
+                </li>
+
+                <li> <!-- Lien vers le formulaire pour me contactez -->
+                    <a href="index.html"> Me contacter </a> 
+                </li>
+            </ul>
+        </nav>
+    </header>
     
 </body>
 </html>
