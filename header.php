028e346b64beb591f2203a813ea26e9857032cff
Enola Tutellier <enola.tutellier@hotmail.com>
2022-05-31 16:00:55 +0200

    formulaire,photo

--- header.php
+++ header.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!--  <a href='https://fr.freepik.com/vecteurs/texture-graphique'>Texture graphique vecteur créé par almumtazza - fr.freepik.com</a> --> 
-    <title>Header</title>
+
+        <title>
+            Header
+        </title>
 </head>
 <body>
 <header>
